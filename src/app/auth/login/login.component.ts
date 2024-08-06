2dba9097d2e905429f863677c3ebfee6257d21dc
Snoopy8403 <77445381+Snoopy8403@users.noreply.github.com>
2024-08-06 21:21:03 +0200

    Alkalmazásfejlesztés 2. 87 Out service megírása 1.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email?: string;
+  password?: string;
+
+  login() {
+    
+  }
 
 }
--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
